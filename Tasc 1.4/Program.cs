94aa8b9f614b2c404f28a721b8fe67bc18fbe1db
Babron <Babron@DESKTOP-6UE8VV6>
2025-11-15 14:44:08 +0200

    Add project files.

---
+++ Tasc 1.4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int V = a * b * c;
+        int S = 2 * (a * b + b * c + a * c);
+
+        Console.WriteLine("Об'єм V = " + V);
+        Console.WriteLine("Площа поверхні S = " + S);
+    }
+}
+ 2
