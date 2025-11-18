0232f32d3cc51009e659272fad27609ad0bda44c
Zeus223-yc <slavi4813@gmail.com>
2025-11-18 18:20:21 +0200

    Add files via upload

---
+++ task 3,2№4/task 3,2№4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть шлях до файлу:");
+        string path = Console.ReadLine();
+
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("Файл не знайдено!");
+            return;
+        }
+
+     
+        List<int> numbers = new List<int>();
+        foreach (var part in File.ReadAllText(path).Split(new[] { ' ', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (int.TryParse(part, out int n))
+                numbers.Add(n);
+        }
+
+       
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            int num = numbers[i];
+
+           
+            result.Add(num);
+
+         
+            if ((i + 1) % 2 == 1 && num % 2 == 0)
+            {
+                
+                result.Add(num);
+            }
+        }
+
+     
+        File.WriteAllText(path, string.Join(" ", result));
+
+        Console.WriteLine("Готово! Парні числа на непарних місцях продубльовано.");
+    }
+}
---
+++ task 3№4/task 3№4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Введіть текстовий рядок:");
+        string text = Console.ReadLine();
+
+        text = Regex.Replace(text, @"\b\w*[A-Za-z]\w*\b", "");
+
+        text = Regex.Replace(text, @"\d+", "");
+
+        text = Regex.Replace(text, @"\s{2,}", " ").Trim();
+
+        Console.WriteLine("\nРезультат:");
+        Console.WriteLine(text);
+    }
+}
